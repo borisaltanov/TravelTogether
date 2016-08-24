6809f561efecb5867507ccdb0270c2f3f245c49a
borisaltanov <borisaltanov@gmail.com>
2016-08-24 16:28:49 +0300

    join a travel

--- traveltogether/travels/urls.py
+++ traveltogether/travels/urls.py
@@ -3,6 +3,6 @@ from .views import add_travel, detail, index, join_travel
 urlpatterns = [
     url(r'^add_travel/$', add_travel, name='add_travel'),
     url(r'^(?P<travel_id>[0-9]+)/$', detail, name='detail'),
-    url(r'^(?P<travel_id>[0-9]+)/join$', join_travel, name='join_travel'),
+    url(r'^(?P<travel_id>[0-9]+)/join_success$', join_travel, name='join_travel'),
     url(r'^$', index, name='index')
 ]
